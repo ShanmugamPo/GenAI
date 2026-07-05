d614930d967c2694c450a06fca68edd7e4fdcc2e
Shanmugam Pooviah <84885666+ShanmugamPo@users.noreply.github.com>
2026-07-06 00:15:16 +0300

    Refactor grade_system for continuous input and error handling
    Refactored grade_system function to include a loop for continuous input until a valid mark is entered. Improved error handling for non-numeric input.

--- grade_system.py
+++ grade_system.py
@@ -1,21 +1,26 @@
 def grade_system():
-  
-    try:
-        print("Welcome to the Grade System!")
-        mark = float(input("Enter your mark (0 - 100): "))
-        if mark <= 0 or mark > 100:
-            print("Invalid mark. Please enter a value between 0 and 100.")
-        elif mark >= 90 and mark <= 100:
-            print(f"Mark: {mark} -> Grade: A")
-        elif mark >= 80 and mark <= 89:
-            print(f"Mark: {mark} -> Grade: B")
-        elif mark >= 70 and mark <= 79:
-            print(f"Mark: {mark} -> Grade: C")
-        elif mark >= 60 and mark <= 69:
-            print(f"Mark: {mark} -> Grade: D")
-        else:
-            print(f"Mark: {mark} -> Grade: E")
+    print("Welcome to the Grade System!")
+    
+    while True:
+        try:
+            mark = float(input("Enter your mark (0 - 100): "))
+            
+            if mark < 0 or mark > 100:
+                print("Invalid mark. Please enter a value between 0 and 100.")
+                continue
+            
+            elif mark >= 90:
+                print(f"Mark: {mark} -> Grade: A")
+            elif mark >= 80:
+                print(f"Mark: {mark} -> Grade: B")
+            elif mark >= 70:
+                print(f"Mark: {mark} -> Grade: C")
+            elif mark >= 60:
+                print(f"Mark: {mark} -> Grade: D")
+            else:
+                print(f"Mark: {mark} -> Grade: E")
+            break
 
-    except:
-        print("Please enter a numeric valid input.")
+        except ValueError:
+            print("Please enter a valid numeric input.")
 grade_system()
